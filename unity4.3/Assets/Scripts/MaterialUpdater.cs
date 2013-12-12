39b7296dcab871009fed891a39fbcca048227482
Moises Silveira <moises.dornelles@gmail.com>
2013-12-12 06:47:49 -0200

    new giacca

--- unity4.3/Assets/Scripts/MaterialUpdater.cs
+++ unity4.3/Assets/Scripts/MaterialUpdater.cs
@@ -3,43 +3,74 @@ using System.Collections;
 
 public class MaterialUpdater : MonoBehaviour {
 
-	public Material[] materials;
-	public MeshRenderer[] cloths;
-
-	public int y;
+//	public Material[] materials;
+//	public MeshRenderer[] cloths;
+//
+//	public int y;
 
 	public GameObject camera;
 
 	private bool isZoom = false;
 
-	public GameObject model;
+//	public GameObject model;
 
 	// Use this for initialization
-	void Start () {
+//	void Start () {
+//
+//	}
+//
+//	// Update is called once per frame
+//	void Update () 
+//	{
+//
+//	}
 
-	}
 
-	// Update is called once per frame
-	void Update () 
-	{
+//	float ROTSpeed = 5f;
+//	float min = -52.0f;
+//	float max = 10.0f;
+//	
+//	void Start()
+//	{
+//		min = Camera.main.fov + min;
+//		max = Camera.main.fov + max;
+//	}
+//	
+//	void Update()
+//	{
+//		if(Camera.main.fov <= min)
+//			Camera.main.fov = min;
+//		if(Camera.main.fov >= max)
+//			Camera.main.fov = max;
+//		
+//		Camera.main.fov -= Input.GetAxis("Mouse ScrollWheel") * ROTSpeed;
+//	}
+//	
 
-	}
+
+
+	
 
 	void OnGUI()
 	{
-		if(GUI.Button(new Rect(10,10, 100, 50), "Zoom"))
+//		if(GUI.Button(new Rect(10,70, 100, 50), "Zoom"))
+//		{
+//			if(!isZoom)
+//			{
+//				camera.transform.position = new Vector3(camera.transform.position.x, camera.transform.position.y, -7.15f);
+//				isZoom = true;
+//			}
+//			else
+//			{
+//				camera.transform.position = new Vector3(camera.transform.position.x, camera.transform.position.y, -8f);
+//				isZoom = false;
+//			}
+//
+//		}
+		
+		if(GUI.Button(new Rect(10,10, 100, 50), "Esc"))
 		{
-			if(!isZoom)
-			{
-				camera.transform.position = new Vector3(camera.transform.position.x, camera.transform.position.y, -7.15f);
-				isZoom = true;
-			}
-			else
-			{
-				camera.transform.position = new Vector3(camera.transform.position.x, camera.transform.position.y, -8f);
-				isZoom = false;
-			}
-
+			Application.Quit();
 		}
 	}
 
---
+++ unity4.3/Assets/Scripts/Zoom.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Zoom : MonoBehaviour
+{
+
+	public float distance;
+	public float sensitivityDistance = -7.5f;
+	public float damping = 2.5f;
+	public float min = -15;
+	public float max = -2;
+	private Vector3 zdistance;
+	
+	void  Start ()
+	{
+		distance = -10f;
+		distance = transform.localPosition.z;
+		zdistance = this.transform.localPosition;
+	}
+	void  Update ()
+	{
+		distance -= Input.GetAxis("Mouse ScrollWheel") * sensitivityDistance;
+		distance = Mathf.Clamp(distance, min, max);
+		zdistance.z = Mathf.Lerp(transform.localPosition.z, distance, Time.deltaTime * damping);
+		transform.localPosition = zdistance;// new Vector3(transform.localPosition.x,transform.localPosition.y,zdistance.z);
+	}
+}
